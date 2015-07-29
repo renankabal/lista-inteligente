b6e2c01a3fb325456e7b350a982ec0edfd318d3a
Renan Jhonatha <renan@roesc.com>
2015-07-29 13:24:45 -0300

    Refatorado

--- index.php
+++ index.php
@@ -20,9 +20,14 @@
 }
 </style>
 <body>
+    <p><br></p>
+    <p><br></p>
+    <p><br></p>
+    <p><br></p>
+    <p><br></p>
 	<center>
-		<textarea id="input_resultado"></textarea>
-	</center>
+		<textarea id="input_resultado" rows="10" style="width:80%;"></textarea>
+	
 	<p></p>
 	<p></p>
 	<p></p>
@@ -30,24 +35,31 @@
 	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#lista">
   		Ver lista
 	</button>
-<!-- Modal -->
-<div class="modal fade" id="lista" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Lista</h4>
-      </div>
-      <div class="modal-body">
-        <code>Clique na lista que deja selecionar as informações:</code><br>
-        	<div class='bs-callout' id='bs-callout-danger' style="cursor: pointer;" onmouseover="javascript:this.style.backgroundColor='#A9C3F9'" onmouseout="javascript:this.style.backgroundColor=''">A</div>
-            <div class='bs-callout' id='bs-callout-danger' style="cursor: pointer;" onmouseover="javascript:this.style.backgroundColor='#A9C3F9'" onmouseout="javascript:this.style.backgroundColor=''">B</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
-      </div>
+    </center>
+    <!-- Modal -->
+    <div class="modal fade" id="lista" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Lista</h4>
+                </div>
+                <div class="modal-body">
+                    <code>Clique na lista que deseja selecionar as informações:</code><br>
+            	    <div class='bs-callout' id='bs-callout-danger' style="cursor: pointer;" onmouseover="javascript:this.style.backgroundColor='#A9C3F9'" onmouseout="javascript:this.style.backgroundColor=''">Aqui irá conter o conteudo do documento</div>
+                    <div class='bs-callout' id='bs-callout-danger' style="cursor: pointer;" onmouseover="javascript:this.style.backgroundColor='#A9C3F9'" onmouseout="javascript:this.style.backgroundColor=''">Neste outro será um outro texto a ser selecionado</div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+<br>
+<br>
+<br>
+<div class="alert alert-success" role="alert">
+    Clique no botão <b>ver lista</b> ou aperte a tecla <b>F5</b> para aparecer a modal com as informações!
 </div>
 </body>
 	<script src="public/jquery/jquery-1.11.3.min.js"></script>
@@ -59,7 +71,7 @@
 				$(this).text());
 		});
 
-		 $(document).bind('keydown', function(e) {
+		$(document).bind('keydown', function(e) {
             if (e.which == 118) { // 118 = F7 http://odesenvolvedor.andafter.org/publicacoes/tabela-de-key-codes-para-javascript_1464.html
                $("#lista").modal("show");
             }
